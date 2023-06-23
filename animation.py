4e09b264e267cf323996f5e650851737c0af5f7d
Lucas Sampaio <111385611+Like-a-stone@users.noreply.github.com>
2023-06-23 13:09:48 -0300

    Initial commit

---
+++ animation.py
@@ -0,0 +1,18 @@
+import time
+class animation_time:
+    def __init__(self) -> None:
+        self.delay=""
+
+    def loading():
+        for i in range(2):
+            print("Carregando  ", end="\r")
+            time.sleep(0.5)
+            print("Carregando > ", end="\r")
+            time.sleep(0.5)
+            print("Carregando >> ", end="\r")
+            time.sleep(0.5)
+            print("Carregando >>> ", end="\r")
+            time.sleep(0.5)
+         
+
+animation_time.loading()        
